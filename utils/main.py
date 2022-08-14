a6a7fd70fa4739b5da679093914bcda867a3f7d3
RenaLio <100777033+RenaLio@users.noreply.github.com>
2022-08-14 22:26:56 +0800

    Update main.py

--- utils/main.py
+++ utils/main.py
@@ -1,6 +1,10 @@
 import freev2
+import qqfreev2
 import freess
+import freess2
 
 if __name__=='__main__':
     freev2.get_conf()
+    qqfreev2.get_conf()
     freess.get_conf()
+    freess2.get_conf()
